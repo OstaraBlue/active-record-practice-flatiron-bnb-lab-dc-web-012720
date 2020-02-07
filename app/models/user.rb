957046dcb7b75fedfca8f6f950cfa1ecbf4769d2
rjwilson0531 <rjwilson0531@gmail.com>
2020-02-07 15:36:49 -0500

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
     has_many :listings, :foreign_key => "host_id"
     has_many :reservations, through: :listings, :foreign_key => "host_id"
 
-    #Guests
+    #Guests done
     has_many :trips, class_name: "Reservation", :foreign_key => "guest_id"
     has_many :reviews, :foreign_key => "guest_id"
 end
